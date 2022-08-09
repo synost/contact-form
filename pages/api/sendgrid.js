c56a0d0ae52675a8a7f231485f3b451adb8b6575
synost <107317938+synost@users.noreply.github.com>
2022-08-09 17:57:18 +0200

    Update sendgrid.js

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -1,27 +1,25 @@
-import nodemailer from 'nodemailer-react'
-
-async function sendEmail(req, res, ) {
-  try {
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      port: 587,
-      auth: {
-          user: 'lea81@ethereal.email',
-          pass: 'cU7mJqBdUAAQEDtVyv'
-      }
-    });
-    let info = await transporter.sendMail({
-      from: '"James Swanson" <foo@example.com>',
-      to: "rickie.gutkowski59@ethereal.email", // Test email address
-      subject: "I love SMTP!",
-      text: "Here's a text version of the email.",
-      html: "Here's an <strong>HTML version</strong> of the email.",
-    });
-  }
-  catch (error) {
-      console.log(error);
-     return res.status(error.statusCode || 500).json({ error: error.message });
-   }
-  return res.status(200).json({ error: "" });
+const nodemailer = require("nodemailer"); // Require the Nodemailer package
+async function main() {
+  // SMTP config
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email", //
+    port: 587,
+    auth: {
+      user: "lea81@ethereal.email", // Your Ethereal Email address
+      pass: "cU7mJqBdUAAQEDtVyv", // Your Ethereal Email password
+    },
+  }); // Send the email
+  let info = await transporter.sendMail({
+    from: '"James Swanson" <foo@example.com>',
+    to: "rickie.gutkowski59@ethereal.email", // Test email address
+    subject: "I love SMTPOP!",
+    text: "Here's a text version of the email.",
+    html: "Here's an <strong>HTML version</strong> of the email.",
+  });
+  console.log("Message sent: %s", info.messageId); // Output message ID
+  console.log("View email: %s", nodemailer.getTestMessageUrl(info)); // URL to preview email
 }
-export default sendEmail;
+// Catch any errors and output them to the console
+main().catch(console.error);
+
+COPY
