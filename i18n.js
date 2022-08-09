3b354f678bfa9db9e1a66b34738727aa8eb7cfaf
synost <107317938+synost@users.noreply.github.com>
2022-08-10 00:35:05 +0200

    dev

--- i18n.js
+++ i18n.js
@@ -1,5 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+//import XHR from "i18next-http-backend";
 import common_fr from "./translations/fr-FR/common.json";
 import common_en from "./translations/en/common.json";
 
@@ -15,17 +17,26 @@ const resources = {
   },
 };
 
+const options = {
+  order: ['querystring', 'navigator'],
+  lookupQuerystring: 'lng'
+}
+
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
   .init({
+    // lng: 'en' // <--- turn off for detection to work
+    detection: options,
     resources,
-    lng: "cimode", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
-
+    ns: ['common'],
+    defaultNS: 'common',
+    fallbackLng: 'en',
+    supportedLngs: ['fr', 'en'],
     interpolation: {
-      escapeValue: false // react already safes from xss
-    }
-  });
-
-  export default i18n;
+      escapeValue: false,
+    },
+    debug: false,
+  })
+  
+export default i18n;
