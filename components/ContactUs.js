cde9de0b41bd492e67d88341bee90b4819be8987
synost <107317938+synost@users.noreply.github.com>
2022-08-10 01:02:31 +0200

    DONE

--- components/ContactUs.js
+++ components/ContactUs.js
@@ -119,7 +119,8 @@ export default function ContactUs() {
             {t('welcome.title')}
           </h1>
           <p className="text-sm text-gray-700 mt-4 font-light dark:text-gray-200">
-            {t('welcome.help_up')}
+            {t('welcome.help_up')}<br></br>
+            {t('welcome.help_down')}
           </p>
         </div>
         <form
