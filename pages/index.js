0c0d8d0dbd8f2ca82543af5b3082fa77f5f9822c
synost <107317938+synost@users.noreply.github.com>
2022-08-10 17:47:28 +0200

    dev

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,25 @@
 import Head from "next/head";
 import ContactUs from "../components/ContactUs";
 import '../i18n';
+import { ThemeProvider } from 'next-themes'
 
 export default function Home() {
   return (
-    <div className="">
-      <Head>
-        <title>TheReplay78</title>
-        <meta
-          name="description"
-          content="TheReplay78 - Entrons en contact."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <ThemeProvider attribute="class">
+      <div className="">
+        <Head>
+          <title>TheReplay78</title>
+          <meta
+            name="description"
+            content="TheReplay78 - Entrons en contact."
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className="">
-        <ContactUs />
-      </main>
-    </div>
+        <main className="">
+          <ContactUs />
+        </main>
+      </div>
+    </ThemeProvider>  
   );
 }
